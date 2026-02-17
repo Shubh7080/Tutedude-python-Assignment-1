54e9007f1dad32cb9971147d64f82f14edaa1afe
Shubh7080 <shubhgupta7080@gmail.com>
2026-02-18 02:12:35 +0530

    Correct file pushed

--- Tutedude_classes.py
+++ Tutedude_classes.py
@@ -1,8 +1,42 @@
-# name = input("Enter your name: ")
-# age = int(input("Enter your age: "))
-# print(name)
-# print(age)
+num1 = 10
+num2 = 20
 
-a = float(input("enter First side"))
-b = float(input("enter First side"))
-c = float(input("enter First side"))
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+addition = num1 + num2
+print("Addition:", addition)
+
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+Subtraction = num1 - num2
+print("Subtraction:", Subtraction)
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+Multiplication = num1 * num2
+print("Multiplication:", Multiplication)
+
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+Division = num1/num2
+print("Division:", Division)
+
+
+a = input("Enter first name: ")
+b = input("Enter last name: ")
+
+Full_name = a+b
+print("Hello,",Full_name +"!"+ " welcome to the python program.")
+
+
+year = int(input("Enter current year: "))
+age = int(input("enter your age: "))
+
+current_age = year - age
+print("You were born in the year:", current_age)
+
+
+
+
